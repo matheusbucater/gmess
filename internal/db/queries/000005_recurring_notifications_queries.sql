1c2c789932a2433f9cc53837abf793b83bb82179
Matheus Bucater <matheusp.bucater@gmail.com>
2025-05-26 12:45:32 -0300

    quick fix

--- internal/db/queries/000005_recurring_notifications_queries.sql
+++
@@ -1,5 +0,0 @@
--- name: GetSimpleNotificationByNotificationId :one
-SELECT * FROM simple_notifications WHERE notification_id = ?;
-
--- name: CreateSimpleNotification :exec
-INSERT INTO simple_notifications (notification_id, trigger_at) VALUES (?, ?);
